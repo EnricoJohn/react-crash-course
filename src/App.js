4a9fcac868c78829437bf8e3bae88be519e210e5
Enrico John <hdpjenrico@gmail.com>
2019-01-09 00:35:15 -0300

    blahblahblah
    blahblahblah2

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/header'
+import About from './components/pages/About'
+import './App.css';
+import uuid from 'uuid'
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    todos: [
+      /*{
+        id: uuid.v4(),
+        title: 'Take out the trash',
+        completed: false
+      }
+      */
+    ]
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10').then(res => this.setState({ todos: res.data }))
+  }
+
+  //Toggle Complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+
+  //Add Todo
+  AddTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
+  render() {
+
+    return (
+      <Router>
+        <div className="App">
+          <div className='container'>
+            <Header />
+            <Route exact path='/' render={props => (
+              <React.Fragment>
+                <AddTodo AddTodo={this.AddTodo} />
+                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                  delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path='/about' component={About} />
+
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
